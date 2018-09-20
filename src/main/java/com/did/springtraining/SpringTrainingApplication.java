eb400741e5936a3d37ef26bcde99e2c04a8907aa
Ogun Adsay <ogun.adsay@emakina.com.tr>
2018-09-20 15:21:29 +0300

    Read configuration automatically with config prefix

--- src/main/java/com/did/springtraining/SpringTrainingApplication.java
+++ src/main/java/com/did/springtraining/SpringTrainingApplication.java
@@ -2,11 +2,18 @@ package com.did.springtraining;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
+@EnableConfigurationProperties
 public class SpringTrainingApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringTrainingApplication.class, args);
+
+		ApplicationContext ctx = SpringApplication.run(SpringTrainingApplication.class, args);
+
+		MyAppConfig config = (MyAppConfig) ctx.getBean("myAppConfig");
+		System.out.println(config.toString());
 	}
 }
