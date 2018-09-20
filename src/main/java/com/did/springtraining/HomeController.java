6b36dc46285783ca8937ab756dff83806bc66f23
Ogun Adsay <ogun.adsay@emakina.com.tr>
2018-09-20 15:02:18 +0300

    Application properties

--- src/main/java/com/did/springtraining/HomeController.java
+++ src/main/java/com/did/springtraining/HomeController.java
@@ -1,12 +1,23 @@
 package com.did.springtraining;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
+
+    @Value("${homeController.msg}")
+    private String homeControllerMsg;
+
+    @Value("${app.description}")
+    private String appName;
+
+    @Value("${my.secret}")
+    private String UUID;
+
     @RequestMapping("/")
     public String home(){
-        return "Hellos world!";
+        return UUID;
     }
 }
