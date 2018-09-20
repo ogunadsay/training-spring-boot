0022e976ee169136ee7c0caa30632031d40595b6
Ogün Adsay(IZM LT-175) <ogunadsay@hotmail.com>
2018-09-20 12:25:17 +0300

    Devtools added

--- src/main/java/com/did/springtraining/HomeController.java
+++ src/main/java/com/did/springtraining/HomeController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @RequestMapping("/")
     public String home(){
-        return "Hello world!";
+        return "Hellos world!";
     }
 }
